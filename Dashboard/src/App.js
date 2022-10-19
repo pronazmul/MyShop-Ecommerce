f756f40c663b6efe617199c6daa7ade85717be0e
pronazmul <developernazmul@gmail.com>
2022-10-20 01:07:04 +0600

    Dashboard Page INitialize

--- Dashboard/src/App.js
+++ Dashboard/src/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import NotFound from './pages/NotFound'
 
 const App = () => {
-  return <div>hello programmers how are you</div>
+  return (
+    <Layout>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Layout>
+  )
 }
 
 export default App
---
+++ Dashboard/src/components/layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Layout = ({ children }) => {
+  if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+  return <>{children}</>
+}
+
+export default Layout
